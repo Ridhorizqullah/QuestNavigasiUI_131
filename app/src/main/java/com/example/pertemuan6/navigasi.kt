0c67d1e38699c3188880cb0e693f74328204f316
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-06 16:43:03 +0700

    menambahkan file navigasi.kt

---
+++ app/src/main/java/com/example/pertemuan6/navigasi.kt
@@ -0,0 +1,21 @@
+package com.example.pertemuan6
+
+import androidx.compose.material3.Scaffold
+import com.example.pertemuan6.view.composable
+
+enum class Nvigasi {
+    Formulirku,
+
+    Detail
+
+}
+
+@composable
+Fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifeir: modifier
+){
+    Scaffold {
+
+    }
+}
