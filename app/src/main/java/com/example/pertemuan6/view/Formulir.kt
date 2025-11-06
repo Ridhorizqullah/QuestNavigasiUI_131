62228e5be707a881b68a13d4c94b0a88290cf361
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-06 15:36:52 +0700

    tambah FormIsian dengan jenis kelamin

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -1,2 +1,28 @@
 package com.example.pertemuan6.view
 
+import android.R
+
+
+@composable
+fun FormIsian(
+    jenisK:List<string> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
