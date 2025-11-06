4f5776c79219a3b55e0ac65235a0456319913efe
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-06 15:42:22 +0700

    menambah HomeScreen dengan Scaffold dan TopAppBar

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -1,6 +1,10 @@
 package com.example.pertemuan6.view
 
 import android.R
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 
 @composable
@@ -8,6 +12,17 @@ fun FormIsian(
     jenisK:List<string> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    scaffold (modifier=modifier,
+        topBar={
+            topAppBar(
+                tittle = {Text(text= stringResource(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.purple_500))
+            )
+        }
+    ){
+        }
 
 
 
