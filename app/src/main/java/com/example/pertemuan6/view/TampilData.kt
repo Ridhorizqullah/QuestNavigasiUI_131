199ac96cd380e600c9bfb91c2198e2747b83bb32
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-06 16:24:47 +0700

    tambah Scaffold dengan TopAppBar berjudul Tampil dan warna teal_700

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -1,11 +1,34 @@
 package com.example.pertemuan6.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
 @composable
 fun TampilanData(
     onBackBtnClick:()-> Unit
 ){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), second = ""),
+        Pair(stringResource(id = R.string.alamat), second = "Contoh Alamat")
+}{
+    Scaffold (modifier = Modifier,
+        topBar = {
+            topAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil)
+                        color = color.white
+                    )},
+                colors = TopAppBarDefaulfaults
+                    .mediumTopAppBarColors(containerColor = color.Resource
+                              (id=R.color.teal_700)
+                    )
+            )
+        }){
 
-
-
-
+    }
 }
