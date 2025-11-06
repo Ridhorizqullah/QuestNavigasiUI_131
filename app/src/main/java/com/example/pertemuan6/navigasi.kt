27f34e97fa0b393c2d16c7a701065038fb542d78
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-06 16:57:52 +0700

    tambah fungsi cancelAndBackToFormulirku untuk navigasi kembali ke Formulirku

--- app/src/main/java/com/example/pertemuan6/navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/navigasi.kt
@@ -1,6 +1,15 @@
 package com.example.pertemuan6
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan6.view.FormIsian
+import com.example.pertemuan6.view.TampilanData
 import com.example.pertemuan6.view.composable
 
 enum class Nvigasi {
@@ -10,22 +19,39 @@ enum class Nvigasi {
 
 }
 
-@composable
-Fun DataApp(
+@Composable
+fun DataApp(
     navController: NavHostController = rememberNavController(),
-    modifeir: modifier
-){
+    modifier: Modifier = Modifier
+) {
     Scaffold { isiRuang ->
         NavHost(
             navController = navController,
             startDestination = Nvigasi.Formulirku.name,
+            modifier = modifier.padding(isiRuang)
+        ) {
+            composable(route = Nvigasi.Formulirku.name) {
+                FormIsian(
+                    onSubmitBtnClick = {
+                        navController.navigate(Nvigasi.Detail.name)
+                    }
+                )
+            }
 
-            modifier = modifier.padding(paddingValues = isiRuang)){
-            composable(route = Nvigasi.Formulirku.name){
-
+            composable(route = Nvigasi.Detail.name) {
+                TampilanData (
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
             }
         }
-        )
-
     }
 }
+
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+){
+    navController.popBackStack(route = Nvigasi.Formulirku.name, inclusive = false)
+}
+
