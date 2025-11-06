40f55c583684819b2e215168fe6cfb32db8aab21
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-06 15:58:55 +0700

    menambhakna HorizontalDivider dengan thickness dan color

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -41,7 +43,14 @@ fun FormIsian(
                     .padding(top = 10.dp)
                     .width(width= 250.dp),
                 label = {Text(text = "Nama Lengkap")}
+                onvaluechange
             )
+            HorizontalDivider(modifier = modifier
+                .padding(all=20.dp)
+                .width(width = 250.dp), thickness = Thickness, color = coloer.Red)
+
+
+
 
 
 
